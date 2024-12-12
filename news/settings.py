0c929870bd8a8690222370e492e9f7b78e834ce9
AmalSultanov <pogboom1014@gmail.com>
2024-12-12 19:57:36 +0500

    added static, media related settings

--- news/settings.py
+++ news/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,5 +81,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = (os.path.join(BASE_DIR / 'assets', ),)
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
