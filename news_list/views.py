9d59a43a80c5138136fb0f351925657c928b250f
AmalSultanov <pogboom1014@gmail.com>
2024-12-12 20:04:44 +0500

    added registration handling

--- news_list/views.py
+++ news_list/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.views import View
 
+from .forms import RegistrationForm
 from .models import News, NewsCategory
 
 
@@ -24,3 +28,29 @@ def category_page(request, pk):
     context = {'category': category, 'news': news}
 
     return render(request, 'category.html', context)
+
+
+class Register(View):
+    template_name = 'registration/register.html'
+
+    def get(self, request):
+        context = {'form': RegistrationForm}
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            username = form.clean_username()
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password2')
+            user = User.objects.create_user(username=username,
+                                            email=email,
+                                            password=password)
+            user.save()
+            login(request, user)
+
+            return redirect('/')
+
+        context = {'form': form, 'message': 'Password or email is incorrect'}
+        return render(request, self.template_name, context)
