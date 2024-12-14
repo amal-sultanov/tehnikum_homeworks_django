40d41b856abf7419130937a2d56d63c43f1e4c16
AmalSultanov <pogboom1014@gmail.com>
2024-12-14 23:43:56 +0500

    added str methods to models

--- news_list/models.py
+++ news_list/models.py
@@ -5,9 +5,15 @@ class NewsCategory(models.Model):
     title = models.CharField(max_length=32)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+
 
 class News(models.Model):
     title = models.CharField(max_length=64)
     description = models.TextField()
     category = models.ForeignKey(NewsCategory, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
