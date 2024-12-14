31f872a618c1dc64f72298630a68e0d6283498c0
AmalSultanov <pogboom1014@gmail.com>
2024-12-14 23:49:35 +0500

    added wishlist to admin panel

--- news_list/admin.py
+++ news_list/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import NewsCategory, News
+from .models import NewsCategory, News, Wishlist
 
 admin.site.register(NewsCategory)
 admin.site.register(News)
+admin.site.register(Wishlist)
