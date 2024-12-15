0d03e4810a6a8a91dbbaf9ff68d4be40615bc1c4
AmalSultanov <pogboom1014@gmail.com>
2024-12-15 11:12:48 +0500

    added register, logout and wishlist related paths

--- news_list/urls.py
+++ news_list/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.homepage),
     path('news/<int:pk>', views.news_page),
     path('category/<int:pk>', views.category_page),
-    path('register', views.Register.as_view())
+    path('register', views.Register.as_view()),
+    path('logout', views.logout_view),
+    path('add-to-wishlist/<int:pk>', views.add_to_wishlist),
+    path('delete-from-wishlist/<int:pk>', views.delete_from_wishlist),
+    path('wishlist', views.wishlist_page),
 ]
